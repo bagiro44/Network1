ec1497c8655007b9f3dfd9185909fcda0a1f4d79
bagiro44 <dmitriyremezov@MacBook-Air-Dmitriy.local>
2013-08-28 15:05:08 +0400

    Initial Commit

---
+++ Network1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Network1
+//
+//  Created by Dmitriy Remezov on 28.08.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Network1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Network1
+//
+//  Created by Dmitriy Remezov on 28.08.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
